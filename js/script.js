530594814a7260552de161d03d0f0e951a479677
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2022-06-07 15:48:38 -0300

    Update script.js
    changed variables from var to let and const

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ function convert() {
 	// Access and change binary variable
 	text = String(document.getElementById("text").value);
 
-	var alphabet = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", " "]
-	var morse = ["-----",".----","..---","...--","....-",".....","-....","--...","---..","----.",".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..", " / "]
+	const alphabet = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", " "]
+	const morse = ["-----",".----","..---","...--","....-",".....","-....","--...","---..","----.",".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..", " / "]
 
-	var textArray = text.toUpperCase().split("");
-	var morseArray = []
-	for (var i = 0; i < textArray.length; i++){
-		for (var j = 0; j < alphabet.length; j++){
+	let textArray = text.toUpperCase().split("");
+	let morseArray = []
+	for (let i = 0; i < textArray.length; i++){
+		for (let j = 0; j < alphabet.length; j++){
 			if (textArray[i] == alphabet[j]){
 				morseArray.push(morse[j]);
 			}
